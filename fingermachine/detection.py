7c6f27f08ab1c05fdc6f2a386010d73dba7346ac
Alexis Métaireau <alexis@notmyidea.org>
2013-03-16 14:19:19 -0700

    changes on the old stuff with opencv

--- fingermachine/detection.py
+++ fingermachine/detection.py
@@ -28,10 +28,12 @@ def detect_shapes(img_path):
     Returns a tuple of (markers, path).
     """
     gray = cv2.imread(img_path, 0)
+    # gray = cv2.equalizeHist(gray)
 
-    _, threshold = cv2.threshold(gray, 127, 255, 1)
+    _, threshold = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY_INV)
 
-    contours, _ = cv2.findContours(threshold, 1, 2)
+    contours, _ = cv2.findContours(threshold, mode=cv2.RETR_EXTERNAL,
+                                   method=cv2.CHAIN_APPROX_SIMPLE)
 
     shapes = {
         3: 'borders',  # triangles
@@ -42,16 +44,37 @@ def detect_shapes(img_path):
     markers = defaultdict(list)
     path = None
 
+    possible_paths = []
+
+    # first iteration, get the start, end and borders.
     for cnt in contours:
-        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+        approx = cv2.approxPolyDP(cnt,
+                    epsilon=0.04 * cv2.arcLength(cnt, True),
+                    closed=True)
+
         sections = len(approx)
 
         if sections in shapes:
             center = get_center(cnt)
             if center is not None:
                 markers[shapes[sections]].append(center)
-
         elif sections > max(shapes.keys()):
+            possible_paths.append(cnt)
+
+    # sort by x and then y.
+    # XXX ensure we have three "borders".
+    borders = sorted(markers['borders'], key=lambda x: (x[0], x[1]))
+    top_left = borders[0]
+    bottom_left = borders[1]
+    top_right = borders[2]
+
+    for cnt in possible_paths:
+        # check the path is inside the paper
+        center = get_center(cnt)
+        if (top_left <= center <= top_right
+                and center[0] > bottom_left[0]
+                and center[1] < bottom_left[1]):
+
             # That's probably the racing path.
             path = cnt
             print "path detected"
@@ -76,9 +99,16 @@ if __name__ == '__main__':
     # eventually do something with it.
     img = cv2.imread(img_path)
 
-    for values in markers.values():
+    colors = {'borders': (0, 0, 255),
+              'start': (0, 255, 0),
+              'end': (255, 0, 0)}
+
+    for key, values in markers.items():
+        color = colors[key]
         for center in values:
-            cv2.circle(img, center, 2, (0, 0, 255), 5)
+            cv2.circle(img, center, 2, color, 5)
+
+    print markers
 
     if path is not None:
         cv2.drawContours(img, [path], 0, (0, 255, 255), -1)
